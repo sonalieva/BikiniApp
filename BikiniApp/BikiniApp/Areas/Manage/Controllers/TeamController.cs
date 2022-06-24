d6af9d0bd47d9629823de0be3cf9ff82cfc82575
sonalieva <708v8lg@code.edu.az>
2022-06-24 13:06:14 +0400

    created

---
+++ BikiniApp/BikiniApp/Areas/Manage/Controllers/TeamController.cs
@@ -0,0 +1,24 @@
+﻿using BikiniApp.DAL;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikiniApp.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class TeamController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TeamController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BikiniApp/BikiniApp/Models/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikiniApp.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        [MaxLength(25)]
+        public string Title { get; set; }
+        [MaxLength(200)]
+        public string Desc { get; set; }
+        public string Image { get; set; }
+    }
+}
